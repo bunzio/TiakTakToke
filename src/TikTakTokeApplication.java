a13c5e49bae2211dc083d32e587e541149f72959
Bunzio <bunzeladam@gmail.com>
2018-04-04 06:05:52 +0200

    Utworzenie projektu

---
+++ src/TikTakTokeApplication.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Bunzio on 04.04.2018
+ * Contact: bunzeladam@gmail.com
+ */
+public class TikTakTokeApplication {
+    public static void main(String[] args){
+        System.out.println("HelloWorld");
+    }
+}
