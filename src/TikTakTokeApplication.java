75a1d1961bf69ddbd44b145a5cefc6a06c20b5d3
Bunzio <bunzeladam@gmail.com>
2018-04-04 21:43:51 +0200

    Interfej graficzny aplikacji

--- src/TikTakTokeApplication.java
+++ src/TikTakTokeApplication.java
@@ -1,9 +1,33 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
 /**
  * Created by Bunzio on 04.04.2018
  * Contact: bunzeladam@gmail.com
  */
-public class TikTakTokeApplication {
-    public static void main(String[] args){
-        System.out.println("HelloWorld");
+public class TikTakTokeApplication extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("Kółko i Krzyżyk by bunzio v1.0.0");
+        FXMLLoader fxmlLoader = new FXMLLoader(TikTakTokeApplication.class.getClassLoader().getResource("gui/gui" +
+                ".fxml"));
+        Parent layout = fxmlLoader.load();
+
+        Scene scene = new Scene(layout);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+
     }
+
+
 }
+
